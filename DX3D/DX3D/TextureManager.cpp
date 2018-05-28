c807ba01339e57d4213db6ecc2cc99a5654bd8bc
jinwonjun <ttwangja@naver.com>
2018-05-28 13:28:51 +0900

    XFILE_READ

--- DX3D/DX3D/TextureManager.cpp
+++ DX3D/DX3D/TextureManager.cpp
@@ -24,19 +24,24 @@ LPDIRECT3DTEXTURE9 TextureManager::GetTexture(string fullPath)
 
 	if (m_mapTexture.find(fullPath) == m_mapTexture.end())
 	{
-		//없으면 생성해서 이름 그대로 넣어주기
-		D3DXCreateTextureFromFileA(g_pDevice, fullPath.c_str(),&m_mapTexture[fullPath]);
+		D3DXCreateTextureFromFileA(g_pDevice, fullPath.c_str(),
+			&m_mapTexture[fullPath]);
 	}
 	return m_mapTexture[fullPath];
+}
+
+LPDIRECT3DTEXTURE9 TextureManager::GetTexture(CString fullPath)
+{
+	CT2CA pszConvertedAnsiString(fullPath);
+	std::string _str(pszConvertedAnsiString);
+	return GetTexture(_str);
 }
 
 void TextureManager::Destroy()
 {
 	for (auto it : m_mapTexture)
 	{
-		//세컨이 텍스쳐 , 퍼스트가 스트링
 		SAFE_RELEASE(it.second);
 	}
-	
 	m_mapTexture.clear();
 }
--- DX3D/DX3D/TextureManager.h
+++ DX3D/DX3D/TextureManager.h
@@ -7,9 +7,11 @@ class TextureManager
 	SINGLETON(TextureManager)
 private:
 	map<string, LPDIRECT3DTEXTURE9> m_mapTexture;
+
 public:
-	LPDIRECT3DTEXTURE9 GetTexture(char * fullPath);
-	LPDIRECT3DTEXTURE9 GetTexture(string  fullPath);
+	LPDIRECT3DTEXTURE9 GetTexture(char* fullPath);
+	LPDIRECT3DTEXTURE9 GetTexture(string fullPath);
+	LPDIRECT3DTEXTURE9 GetTexture(CString fullPath);
 
 	void Destroy();
 };
